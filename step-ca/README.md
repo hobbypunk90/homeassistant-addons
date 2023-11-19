77876fdffa4d32275c73544c3069d77a16f55421
Marcel Hoppe <hoppe.marcel@gmail.com>
2023-11-19 21:53:49 +0100

    Add step-ca

---
+++ step-ca/README.md
@@ -0,0 +1,13 @@
+# step-ca Addon
+
+[step-ca](https://smallstep.com/certificates/) wrapped inside a Homeassistant supervisor addon.
+
+The Open Source step certificates project provides the infrastructure, automations, and workflows to securely create and operate a private certificate authority. Step certificates makes it easy for developers, operators and security teams to manage certificates for production workloads.
+
+![Supports aarch64 Architecture][aarch64-shield] ![Supports amd64 Architecture][amd64-shield] ![Supports armhf Architecture][armhf-shield] ![Supports armv7 Architecture][armv7-shield] ![Supports i386 Architecture][i386-shield]
+
+[aarch64-shield]: https://img.shields.io/badge/aarch64-yes-green.svg
+[amd64-shield]: https://img.shields.io/badge/amd64-yes-green.svg
+[armhf-shield]: https://img.shields.io/badge/armhf-yes-green.svg
+[armv7-shield]: https://img.shields.io/badge/armv7-yes-green.svg
+[i386-shield]: https://img.shields.io/badge/i386-yes-green.svg
---
+++ step-ca/start.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bashio
+
+mkdir -p /data/step
+ln -s /data/step /root/.step
+
+if ! [ -f /data/last_options.json ]; then
+  touch /data/last_options.json
+fi
+
+bashio::config.require 'password'
+
+echo "Debug: $(bashio::config 'debug')"
+
+if [ "" = "" ]; then
+  export STEPDEBUG=1
+fi
+
+echo $(bashio::config 'password') > /tmp/password_file
+
+if [ ! -f /root/.step/config/ca.json ] || ! diff /data/options.json /data/last_options.json &>/dev/null; then
+  rm -fr /data/step/*
+  bashio::log.info 'Initialize step ca ...'
+
+  hostname=$(bashio::host.hostname)
+  network_info=$(bashio::network)
+  ip_addresses=$(echo ${network_info} | jq '[ [.interfaces[]] | .[].ipv4.address[] ] | join(",")' | tr -d '"')
+
+  bashio::log.info $(step ca init --name "${hostname}" \
+                                  --dns "${hostname},${ip_addresses}" \
+                                  --provisioner "homeassistant@${hostname}" \
+                                  --address ":9000" \
+                                  --password-file /tmp/password_file >/dev/null)
+
+  step ca provisioner add homeassistant --type ACME
+  cp /data/options.json /data/last_options.json
+fi
+
+fingerprint=$(cat /data/step/config/defaults.json | grep fingerprint | sed 's/.*"fingerprint": "//; s/",//')
+bashio::log.info "Root fingerprint: ${fingerprint}"
+
+bashio::log.info "Root certificate:"
+cat /data/step/certs/root_ca.crt
+cp  /data/step/certs/root_ca.crt /ssl/root_ca.crt
+
+bashio::log.info "Intermediate certificate:"
+cat /data/step/certs/intermediate_ca.crt
+
+bashio::log.info 'Start step ca ...'
+step-ca --password-file /tmp/password_file /root/.step/config/ca.json
